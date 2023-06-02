0ba6496f6752750cdc7ef0f6d8fa9d6c09de7b99
Adnan <adnanvila@gmail.com>
2023-06-02 15:58:00 +0200

    Preparation for Cast Fragment/Layout

--- app/src/main/java/com/mbresa/moviecatalogapp/MainActivity.kt
+++ app/src/main/java/com/mbresa/moviecatalogapp/MainActivity.kt
@@ -6,6 +6,7 @@ import com.mbresa.moviecatalogapp.databinding.FragmentHomeBinding
 import com.mbresa.moviecatalogapp.databinding.HomeMovieItemBinding
 
 const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+const val CAST_IMAGE_BASE_URL = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
